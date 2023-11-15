05ca45b184733f942a84c10b5436f9f14356c701
sheinezbenbk <sheinezbenbk@gmail.com>
2023-11-15 08:42:33 +0100

    intermission 1 fini (15/11)

--- src/main/kotlin/org/ldv/sprintbootaventure/controller/admin/QualiteControlleur.kt
+++ src/main/kotlin/org/ldv/sprintbootaventure/controller/admin/QualiteControlleur.kt
@@ -1,4 +1,8 @@
 package org.ldv.sprintbootaventure.controller.admin
+import jakarta.persistence.*
 
+@Entity
 class QualiteControlleur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 }
